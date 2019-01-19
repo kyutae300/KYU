ef587207fa08f23c08a8b3da930055c5284f8f84
kyutae300 <kyutae300@nate.com>
2019-01-19 16:44:35 +0900

    first code

--- 6_get_weatherdata/6_get_weatherdata.py
+++ 6_get_weatherdata/6_get_weatherdata.py
@@ -3,4 +3,6 @@ import json
 with open('./6_sampleAPI.json') as data_file:
     data = json.load(data_file)
     toCelsius = data['main']['temp'] - 273
-    print(toCelcius)
+    print(toCelsius)
+
+
